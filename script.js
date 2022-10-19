d1fae01a6a3955cfa3eecb5b88622271ff67b063
Dennis Byberg <dennis.byberg@hotmail.com>
2022-10-19 13:30:17 +0200

    whole program done, now time for design

--- script.js
+++ script.js
@@ -1,23 +1,70 @@
 const rockButton = document.querySelector(".user-rock-btn");
 const paperButton = document.querySelector(".user-paper-btn");
 const scissorButton = document.querySelector(".user-scissor-btn");
+let computerChoiceText = document.querySelector(".computer-choice-text");
+let userChoiceText = document.querySelector(".user-choice-text");
+let winOrLoseText = document.querySelector(".win-or-lose-text");
+let winCounterText = document.querySelector(".win-counter-text");
 
 let computerRandomNumber = 0;
 let userNumber = 0;
-// console.log(`userNumber: ${userNumber}`);
-// console.log(`Computernumber: ${computerRandomNumber}`);
+let winCounter = 0;
+
+function addOneWin() {
+  winCounter++;
+  winCounterText.innerText = `Wins: ${winCounter}`;
+}
 
 rockButton.addEventListener("click", () => {
   userNumber = 1;
   computerRandomNumber = Math.floor(Math.random() * 3) + 1;
+  userChoiceText.innerText = "User: Rock";
+
+  if (userNumber == computerRandomNumber) {
+    computerChoiceText.innerText = "CP: Rock";
+    winOrLoseText.innerText = "Draw!";
+  } else if (computerRandomNumber == 2) {
+    computerChoiceText.innerText = "CP: Paper";
+    winOrLoseText.innerText = "You Lose!";
+  } else {
+    computerChoiceText.innerText = "CP: Scissor";
+    winOrLoseText.innerText = "You Win!";
+    addOneWin();
+  }
 });
 
 paperButton.addEventListener("click", () => {
   userNumber = 2;
   computerRandomNumber = Math.floor(Math.random() * 3) + 1;
+  userChoiceText.innerText = "User: Paper";
+
+  if (userNumber == computerRandomNumber) {
+    computerChoiceText.innerText = "CP: Paper";
+    winOrLoseText.innerText = "Draw!";
+  } else if (computerRandomNumber == 1) {
+    computerChoiceText.innerText = "CP: Rock";
+    winOrLoseText.innerText = "You Win!";
+    addOneWin();
+  } else {
+    computerChoiceText.innerText = "CP: Scissor";
+    winOrLoseText.innerText = "You Lose!";
+  }
 });
 
 scissorButton.addEventListener("click", () => {
   userNumber = 3;
   computerRandomNumber = Math.floor(Math.random() * 3) + 1;
+  userChoiceText.innerText = "User: Scissor";
+
+  if (userNumber == computerRandomNumber) {
+    computerChoiceText.innerText = "CP: Scissor";
+    winOrLoseText.innerText = "Draw!";
+  } else if (computerRandomNumber == 1) {
+    computerChoiceText.innerText = "CP: Rock";
+    winOrLoseText.innerText = "You Lose!";
+  } else {
+    computerChoiceText.innerText = "CP: Paper";
+    winOrLoseText.innerText = "You Win!";
+    addOneWin();
+  }
 });
