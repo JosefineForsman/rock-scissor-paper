e7427269469230943e84a40be3c8d527ea3287cc
Josefine Forsman <josefineforsman1@gmail.com>
2022-10-21 11:04:48 +0200

    Color changes + added icons

--- script.js
+++ script.js
@@ -8,22 +8,25 @@ let winCounterText = document.querySelector(".win-counter-text");
 let slider = document.querySelector(".slider");
 let playAgainBtn =document.querySelector(".play-again-btn");
 
-let computerRandomNumber = 0;
-let userNumber = 0;
-let winCounter = 0;
+let computerRandomNumber = 0;  // Computer Score.
+let userNumber = 0;// 1 = Rock | 2 = Paper | 3 = Scissor.
+let winCounter = 0;// Counter variable.
 
+// This function adds one to your score if you win.
 function addOneWin() {
   winCounter++;
   winCounterText.innerText = `Wins: ${winCounter}`;
 }
-
+// This function only toggle the slider to go back and fourth.
 function toggleSlider() {
   slider.classList.toggle("show");
 }
+// When play-again-button is clicked ~> Trigger toggleSlider().
 playAgainBtn.addEventListener ('click', () => {
 toggleSlider();
 })
 
+// What happends if we choose Rock?
 rockButton.addEventListener("click", () => {
   userNumber = 1;
   computerRandomNumber = Math.floor(Math.random() * 3) + 1;
@@ -32,17 +35,21 @@ rockButton.addEventListener("click", () => {
   if (userNumber == computerRandomNumber) {
     computerChoiceText.innerText = "CP: Rock";
     winOrLoseText.innerText = "Draw!";
+    winOrLoseText.style.color ="yellow";
   } else if (computerRandomNumber == 2) {
     computerChoiceText.innerText = "CP: Paper";
     winOrLoseText.innerText = "You Lose!";
+    winOrLoseText.style.color = "red";
   } else {
     computerChoiceText.innerText = "CP: Scissor";
     winOrLoseText.innerText = "You Win!";
+    winOrLoseText.style.color = "green";
     addOneWin();
   }
   toggleSlider();
 });
 
+// What happends if we choose Paper?
 paperButton.addEventListener("click", () => {
   userNumber = 2;
   computerRandomNumber = Math.floor(Math.random() * 3) + 1;
@@ -51,17 +58,21 @@ paperButton.addEventListener("click", () => {
   if (userNumber == computerRandomNumber) {
     computerChoiceText.innerText = "CP: Paper";
     winOrLoseText.innerText = "Draw!";
+    winOrLoseText.style.color ="yellow";
   } else if (computerRandomNumber == 1) {
     computerChoiceText.innerText = "CP: Rock";
     winOrLoseText.innerText = "You Win!";
+    winOrLoseText.style.color = "green";
     addOneWin();
   } else {
     computerChoiceText.innerText = "CP: Scissor";
     winOrLoseText.innerText = "You Lose!";
+    winOrLoseText.style.color = "red";
   }
   toggleSlider();
 });
 
+// What happends if we choose Scissor?
 scissorButton.addEventListener("click", () => {
   userNumber = 3;
   computerRandomNumber = Math.floor(Math.random() * 3) + 1;
@@ -70,12 +81,15 @@ scissorButton.addEventListener("click", () => {
   if (userNumber == computerRandomNumber) {
     computerChoiceText.innerText = "CP: Scissor";
     winOrLoseText.innerText = "Draw!";
+    winOrLoseText.style.color ="yellow";
   } else if (computerRandomNumber == 1) {
     computerChoiceText.innerText = "CP: Rock";
     winOrLoseText.innerText = "You Lose!";
+    winOrLoseText.style.color = "red";
   } else {
     computerChoiceText.innerText = "CP: Paper";
     winOrLoseText.innerText = "You Win!";
+    winOrLoseText.style.color = "green";
     addOneWin();
   }
   toggleSlider();
